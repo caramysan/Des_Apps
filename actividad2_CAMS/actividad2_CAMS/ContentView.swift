afa75149c296e3a586befa07f77c5362fdc52616
caramysan <49528500+caramysan@users.noreply.github.com>
2021-02-10 00:16:03 -0600

    Add files via upload

---
+++ actividad2_CAMS/actividad2_CAMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  actividad2_CAMS
+//
+//  Created by Carlos Mendizabal on 09/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
