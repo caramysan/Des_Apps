70800b4bd6b22bb22634d50250deb8aa0e8de53a
caramysan <49528500+caramysan@users.noreply.github.com>
2021-02-10 01:34:19 -0600

    Add files via upload

---
+++ actividad3_CAMS/actividad3_CAMS.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+/*:
+# Playground - Actividad 3
+* Tipos de datos
+* Asociación de tipos
+* Arreglos y Diccionarios
+*/
+
+
+/*: 
+### Actividad de Tipos de datos
+A) Declarar cuatro variables con tres diferentes tipos de datos, cada uno que corresponda a una numero entero, un numero decimal (flotante), una cadena de texto, realizando la asignación explicita y la asignación inferida
+*/
+var a:Int = 2
+var b:Double = 2.5
+var c = "Carlos Mendizábal"
+var d = 6.76
+/*:
+### Asociación de tipos
+A) Declara el tipo de dato por asociación para un tipo de dato String
+*/
+let e:String = String()
+//: B) Declara el tipo de dato por asociación para un tipo de dato  Número entero.
+let f:Int = Int()
+/*: 
+### Arreglos y Diccionarios
+A) Crea la variable "numeros" de tipo Array con números consecutivos del 1 a 10.
+*/
+var numeros = [1, 2 , 3, 4, 5 ,6 ,7, 8, 9, 10]
+//: B) Crea la variable "diasSemana" de tipo Dictionary con la relación numero:día Ej. 1:"Lunes"
+var diasSemana = [1:"Lunes", 2:"Martes", 3:"Miércoles", 4:"Jueves", 5:"Viernes", 6:"Sábado", 7:"Domingo"]
