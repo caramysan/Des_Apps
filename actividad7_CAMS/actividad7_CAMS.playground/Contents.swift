359676e510aca5984d3795de9bcfebce32273e26
caramysan <49528500+caramysan@users.noreply.github.com>
2021-03-11 00:25:57 -0600

    Add files via upload

---
+++ actividad7_CAMS/actividad7_CAMS.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+/*:
+# Playground - Actividad 7
+* Valor por tipo y por referencia
+* Funciones personalizadas Y Genericos
+* Funciones de la biblioteca Swift para Arreglos
+*/
+
+
+/*: 
+### Valor por tipo y por referencia
+A) Para la colección "var costo_referencia:[Float] = [8.3,10.5,9.9]", aplicar el impuesto del 16% a través de recorrer la colección "costo_referencia" para aplicar el impuesto a cada cantidad, crear una función Impuesto que recibe como parámetro la colección y regrese aplicado el impuesto a cada cantidad.
+*/
+var costo_referencia:[Float] = [8.3,10.5,9.9]
+var indice:Int = 0
+
+func Impuesto (arreglo:[Float]) -> [Float] {
+    var resultado:[Float] = []
+    for i in arreglo {
+        resultado.append(Float(i)*1.16)
+    }
+    return resultado
+}
+
+Impuesto(arreglo: costo_referencia)
+//: B) Crear la función "sumaTres"  que reciba una función con dos valores a sumar y un segundo parametro para sumar el tercer número.
+let suma = { (x:Int, y:Int)->Int in return x+y}
+
+func sumaTres (a:Int, b:Int, c:Int) -> Int {
+    return suma(a, b) + c
+}
+sumaTres(a: 7, b: 6, c: 2)
+/*:
+### Funciones personalizadas y Genéricos
+
+ 
+ A) Generics: Crear la función genérica para intercambiar valores entre dos variables del mismo tipo.
+*/
+
+func cambioString (c:String, d:String) -> (String, String){
+    var c:String = c
+    var d:String = d
+
+    var tempStr = c
+    c = d
+    d = tempStr
+    return (c, d)
+}
+print(cambioString(c: "DatoC", d: "DatoD"))
+//: B) Función personalizada: Crear la función "Transformar" que reciba como parámetro una colección de tipo Int  "var datos = [3,7,9,2]" y una función que transforme cada valor de la coleción en una operación definida fuera de la función, regresando una colección transformada.
+extension Array {
+    func Transformar<T> (inicial:T, acumula:(T, Element) -> T) ->T {
+        var respuesta:T = inicial
+        for valor in self {
+            respuesta = acumula(respuesta, valor)
+        }
+        return respuesta
+    }
+    func newTransform (datos:[Int], datos2:[Int]) -> ([Int], [Int]){
+        var datos:[Int] = datos
+        var datos2:[Int] = datos2
+        
+        var tempArr = datos
+        datos = datos2
+        datos2 = tempArr
+        return (datos, datos2)
+    }
+}
+var datos = [3,7,9,2]
+
+datos.Transformar(inicial: 0) {(a, b) in a+b}
+print(datos.newTransform(datos:[3,7,9,2], datos2:datos.map{$0*5}))
+/*:
+### Biblioteca de Swift
+A) Aplicar la función de librería de Swift "map" para la colección var precios = [4.2, 5.3, 8.2, 4.5] y aplicar el impuesto de 16% y asignarla a la variable "impuesto"
+*/
+var precios = [4.2, 5.3, 8.2, 4.5]
+var impuesto = precios.map { a in return a*1.16}
+impuesto
+//: B) Aplicar la función de la librería de Swift "filter" para la colección resultante "impuesto" del paso A, en donde se obtengas solo los precios mayores a 6.0 y asignarlos a la variable "precio_menor"
+var precio_menor = impuesto.filter {a in a>6.0}
+precio_menor
